e1db99e54da4784f06d178eec75ffecff020d3a8
Naruses <85679021+Naruses@users.noreply.github.com>
2021-06-10 13:20:38 +0300

    Initial commit

---
+++ HomeWorkJune21/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+    void Update()
+    {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 2, -1));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
---
+++ HomeWorkJune21/Assets/Scripts/TestSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private float _maxCount = 3;
+    [SerializeField] private float _maxTime = 3;
+    [SerializeField] private Transform _target;
+    private float _time;
+    private float _count;
+    private bool isOff = false;
+
+    private void Awake()
+    {
+        _time = _maxTime;
+    }
+    void Update()
+    {
+        if (isOff) return;
+
+        _time -= Time.deltaTime;
+        if (_time <= 0)
+            Spawn();
+    }
+    private void Spawn()
+    {
+        var enemy = Instantiate(_enemyPrefab, transform.position, Quaternion.identity).GetComponent<Enemy>();
+        //enemy.Init(_target);
+        _time = _maxTime;
+        _count++;
+        if (_count >= _maxCount)
+            isOff = true;
+    }
+}
